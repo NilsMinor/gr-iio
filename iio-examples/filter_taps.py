ec2030ffb8713e3561753a0ca1805e5193c6e41d
NilsMinor <nilsminor@web.de>
2018-11-26 16:33:14 +0100

    added some examples

--- iio-examples/filter_taps.py
+++ iio-examples/filter_taps.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: Attribute Source Example
-# Author: Travis Collins
-# Generated: Mon Nov 26 12:08:19 2018
+# Title: Filter Taps
+# Generated: Mon Nov 26 11:56:28 2018
 ##################################################
 
 if __name__ == '__main__':
@@ -20,22 +19,24 @@ if __name__ == '__main__':
 from PyQt4 import Qt
 from gnuradio import blocks
 from gnuradio import eng_notation
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio import iio
 from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
+from gnuradio.qtgui import Range, RangeWidget
 from optparse import OptionParser
 import sip
 import sys
 
 
-class attr_source_example(gr.top_block, Qt.QWidget):
+class filter_taps(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Attribute Source Example")
+        gr.top_block.__init__(self, "Filter Taps")
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Attribute Source Example")
+        self.setWindowTitle("Filter Taps")
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
         except:
@@ -52,66 +53,25 @@ class attr_source_example(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "attr_source_example")
+        self.settings = Qt.QSettings("GNU Radio", "filter_taps")
         self.restoreGeometry(self.settings.value("geometry").toByteArray())
 
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 500000000
-        self.fft_size = fft_size = 1024*8
+        self.transition = transition = 1000
+        self.samp_rate = samp_rate = 500*1000
+        self.cutoff_low = cutoff_low = 500
+        
+        self.lp_taps = lp_taps = firdes.low_pass(1.0, samp_rate, cutoff_low, transition, firdes.WIN_HAMMING, 6.76)
+          
+        self.len_taps = len_taps = len(lp_taps)
 
         ##################################################
         # Blocks
         ##################################################
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-        	fft_size/4, #size
-        	samp_rate, #samp_rate
-        	"", #name
-        	1 #number of inputs
-        )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
-        
-        self.qtgui_time_sink_x_0.set_y_label("Amplitude", "")
-        
-        self.qtgui_time_sink_x_0.enable_tags(-1, True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(True)
-        self.qtgui_time_sink_x_0.enable_grid(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
-        
-        if not True:
-          self.qtgui_time_sink_x_0.disable_legend()
-        
-        labels = ["", "", "", "", "",
-                  "", "", "", "", ""]
-        widths = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-                  "magenta", "yellow", "dark red", "dark green", "blue"]
-        styles = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-                   -1, -1, -1, -1, -1]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-        
-        for i in xrange(1):
-            if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
-        
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_f(
-        	fft_size, #size
+        	1024*2, #size
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
         	0, #fc
         	samp_rate, #bw
@@ -122,8 +82,8 @@ class attr_source_example(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
         self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
         self.qtgui_freq_sink_x_0.enable_autoscale(True)
-        self.qtgui_freq_sink_x_0.enable_grid(True)
-        self.qtgui_freq_sink_x_0.set_fft_average(0.1)
+        self.qtgui_freq_sink_x_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
         self.qtgui_freq_sink_x_0.enable_control_panel(False)
         
         if not True:
@@ -151,21 +111,32 @@ class attr_source_example(gr.top_block, Qt.QWidget):
         
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
-        self.iio_device_source_0 = iio.device_source("ip:192.168.2.2", "axi-ad9234", ["voltage0"], "axi-ad9234", [], 0x4000, 1 - 1)
+        self.lp_filter = filter.fir_filter_fff(1, (lp_taps))
+        self.lp_filter.declare_sample_delay(0)
+        self.iio_device_source_0 = iio.device_source("ip:192.168.2.2", "axi-ad9234", ["voltage0"], "axi-ad9234", [], 0x2000, 1 - 1)
+        self._cutoff_low_range = Range(0, 5*1000*1000, 1000, 500, 200)
+        self._cutoff_low_win = RangeWidget(self._cutoff_low_range, self.set_cutoff_low, "cutoff_low", "counter_slider", float)
+        self.top_layout.addWidget(self._cutoff_low_win)
         self.blocks_short_to_float_0 = blocks.short_to_float(1, 32000)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_short_to_float_0, 0), (self.qtgui_freq_sink_x_0, 0))    
-        self.connect((self.blocks_short_to_float_0, 0), (self.qtgui_time_sink_x_0, 0))    
+        self.connect((self.blocks_short_to_float_0, 0), (self.lp_filter, 0))    
         self.connect((self.iio_device_source_0, 0), (self.blocks_short_to_float_0, 0))    
+        self.connect((self.lp_filter, 0), (self.qtgui_freq_sink_x_0, 0))    
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "attr_source_example")
+        self.settings = Qt.QSettings("GNU Radio", "filter_taps")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
+
+    def get_transition(self):
+        return self.transition
+
+    def set_transition(self, transition):
+        self.transition = transition
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -173,16 +144,29 @@ class attr_source_example(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
-    def get_fft_size(self):
-        return self.fft_size
+    def get_cutoff_low(self):
+        return self.cutoff_low
 
-    def set_fft_size(self, fft_size):
-        self.fft_size = fft_size
+    def set_cutoff_low(self, cutoff_low):
+        self.cutoff_low = cutoff_low
+
+    def get_lp_taps(self):
+        return self.lp_taps
+
+    def set_lp_taps(self, lp_taps):
+        self.lp_taps = lp_taps
+        self.set_len_taps(len(self.lp_taps))
+        self.lp_filter.set_taps((self.lp_taps))
+
+    def get_len_taps(self):
+        return self.len_taps
+
+    def set_len_taps(self, len_taps):
+        self.len_taps = len_taps
 
 
-def main(top_block_cls=attr_source_example, options=None):
+def main(top_block_cls=filter_taps, options=None):
 
     from distutils.version import StrictVersion
     if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
